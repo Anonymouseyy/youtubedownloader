d57c761489cad5d45140cf67da45766be6a2f9ff
Anonymouseyy <xieethan2008@gmail.com>
2022-12-05 21:32:33 -0800

    Basic downloading of single videos, need to add progress hooks and quiet verbose

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def what_is(url):
         def debug(msg):
             pass
 
-    ydl_opts = {'skip_download': True, 'ignoreerrors': True, 'quiet': True, "logger": ignoreErrors, 'extract_flat': True}
+    ydl_opts = {'skip_download': True, 'ignoreerrors': True, 'quiet': True, "logger": ignoreErrors, 'extract_flat': True, 'playlistend': 2}
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.extract_info(url, False)
@@ -38,48 +38,67 @@ def what_is(url):
         return 'vid'
 
 
-def start():
-    # Create the Window
-    vid_layout = [[sg.Input('Youtube URL', key='url', font=('Helvetica', 20))]]
-    playlist_layout = [[sg.Input('Youtube Playlist', key='list_url', font=('Helvetica', 20))],
-                       [sg.Input('Start Index', key='start_idx', font=('Helvetica', 20))]]
+def download_video(url, t, path):
+    if t == 'Audio':
+        ydl_opts = {
+            'format': 'mp3/bestaudio/best',
+            'outtmpl': f'{path}/%(title)s.mp3',
+        }
+    else:
+        ydl_opts = {
+            'format': 'mp4/bestvideo/best',
+            'outtmpl': f'{path}/%(title)s.mp4',
+        }
 
-    layout = [[sg.Text('Youtube Downloader', font=('Helvetica', 40))],
-              [sg.Column(vid_layout, key='vid'), sg.Column(playlist_layout, key='playlist', visible=False)],
-              [sg.Push(), sg.Input('Select Folder', key='fpath', font=('Helvetica', 20)), sg.FolderBrowse(font=('Helvetica', 15))],
-              [sg.Combo(['Video', 'Audio'], key='type', font=('Helvetica', 20)),
-               sg.Button('Download', font=('Helvetica', 15)),
-               sg.Button('Playlist Toggle', key='toggle', font=('Helvetica', 15)),
-               sg.Cancel('Exit', font=('Helvetica', 15))]]
-
-    window = sg.Window('Youtube Downloader', layout)
-    cur_layout = 'vid'
-
-    # Start loop
-    while True:
-        # Read window
-        event, values = window.read(timeout=1)
-        if event == sg.WIN_CLOSED or event == 'Exit':
-            break
-
-        if event == 'toggle':
-            if cur_layout == 'playlist':
-                window['playlist'].update(visible=False)
-                window['vid'].update(visible=True)
-                cur_layout = 'vid'
-            else:
-                window['playlist'].update(visible=True)
-                window['vid'].update(visible=False)
-                cur_layout = 'playlist'
-
-        if event == 'Download':
-            if cur_layout == 'vid':
-                print(what_is(values['url']))
-            else:
-                print(what_is(values['list_url']))
-
-    window.close()
-    sys.exit()
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
 
-start()
+# Create the Window
+vid_layout = [[sg.Text('YouTube URL:', font=('Helvetica', 20)), sg.Input(key='url', font=('Helvetica', 20))]]
+playlist_layout = [[sg.Text('Youtube Playlist:', font=('Helvetica', 20)),
+                    sg.Input(key='list_url', font=('Helvetica', 20))],
+                   [sg.Text('Start Index:', font=('Helvetica', 20)), sg.Push(),
+                    sg.Input('Blank for first', key='start_idx', font=('Helvetica', 20), s=15), sg.Push(),
+                    sg.Text('End Index:', font=('Helvetica', 20)), sg.Push(),
+                    sg.Input('Blank for last', key='end_idx', font=('Helvetica', 20), s=15)]]
+
+layout = [[sg.Text('Youtube Downloader', font=('Helvetica', 40))],
+          [sg.Column(vid_layout, key='vid'), sg.Column(playlist_layout, key='playlist', visible=False)],
+          [sg.Push(), sg.Text('Select Folder: ', font=('Helvetica', 20)),
+           sg.Input(key='fpath', font=('Helvetica', 20), s=41), sg.FolderBrowse(font=('Helvetica', 15))],
+          [sg.Combo(['Video', 'Audio'], key='type', font=('Helvetica', 20)),
+           sg.Button('Download', font=('Helvetica', 15)),
+           sg.Button('Playlist Toggle', key='toggle', font=('Helvetica', 15)),
+           sg.Cancel('Exit', font=('Helvetica', 15))]]
+
+window = sg.Window('Youtube Downloader', layout)
+cur_layout = 'vid'
+
+# Start loop
+while True:
+    # Read window
+    event, values = window.read(timeout=1)
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+
+    if event == 'toggle':
+        if cur_layout == 'playlist':
+            window['playlist'].update(visible=False)
+            window['vid'].update(visible=True)
+            cur_layout = 'vid'
+        else:
+            window['playlist'].update(visible=True)
+            window['vid'].update(visible=False)
+            cur_layout = 'playlist'
+
+    if event == 'Download':
+        if values['type'] and values['fpath']:
+            if cur_layout == what_is(values['url']):
+                if cur_layout == 'vid':
+                    download_video(values['url'], values['type'], values['fpath'])
+                else:
+                    pass
+
+window.close()
+sys.exit()
